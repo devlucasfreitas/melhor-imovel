6130a8c6b8d78b5b10cfb7a61654c2f3bb75292f
devlucasfreitas <lucasf.freitas@outlook.com>
2021-09-22 11:10:10 -0300

    Criaçao de cards e responsividade

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,42 +1,80 @@
 import React from 'react';
 import Header from '../../components/header';
 import './home.css';
-import {BiSearchAlt2} from "react-icons/bi";
-import {TiLocation} from "react-icons/ti"
+import { BiSearchAlt2 } from "react-icons/bi";
+import { TiLocation } from "react-icons/ti"
 
 
-function Home(){
-    
-    return(
+function Home() {
+
+    return (
         <>
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             <title> Meu Melhor Imóvel | Home</title>
             <Header />
             <main className="Home">
-                
+
                 <section className="painel">
                     
-                        <form className="formBusca"action="">
-                            <h1 className="titulo">A melhor busca para o seu melhor imóvel</h1>
-                            <select name="alugaOuCompra">
-                                <option value="Alugar">Alugar</option>
-                                <option value="Comprar">Comprar</option>
-                            </select>
-                            <select name="tiposDePropriedade">
-                                <option value="todos">Mostrar todos</option>
-                                <option value="apartamento">Somente Apartamento</option>
-                                <option value="casa">Somente Casa todos</option>
-                            </select>
-                            
-                            <input type="text" placeholder="Digite o bairro ou cidade" />
-                            <button>Enviar</button>
-                        </form>
+                    <form className="formBusca" action="">
+
+                        <h1 className="titulo">A melhor busca para o seu melhor imóvel</h1>
+
+                        <div><button onClick={ativaMenu} className="btnFormBusca ">Começar a Busca</button></div>
+
+                        <div className="selecaoBox ">
+                        <select name="alugaOuCompra">
+                            <option value="Alugar">Alugar</option>
+                            <option value="Comprar">Comprar</option>
+                        </select>
+                        <select name="tiposDePropriedade">
+                            <option value="todos">Mostrar todos</option>
+                            <option value="apartamento">Somente Apartamento</option>
+                            <option value="casa">Somente Casa</option>
+                        </select>
+                        <select name="estado">
+                            <option value="SP">SP</option>
+                        </select>
+                        <select name="cidade">
+                            <option value="SAO PAULO">São Paulo</option>
+                        </select>
+                        <select name="bairro">
+                            <option value="JARDINS">Jardins</option>
+                        </select>
+
+                        <button className="btnBusca">Buscar</button>
+                        </div>
+                    </form>
+                    
+                </section>
+
+
+
+                <section className="preco-medio">
+
+                    <div className="preco-aluguel"><h1>Preço Médio de aluguel</h1></div>
+
+                    <div className="bloco-precos">
+                        <div className="cards sp">
+                            <h2>São paulo</h2>
+                            <h3>R$ 3.476</h3>
+                        </div>
+                        <div className="cards rj">
+                            <h2>Rio de Janeiro</h2>
+                            <h3>R$ 1.808</h3>
+                        </div>
+                        <div className="cards pr">
+                            <h2>Curitiba</h2>
+                            <h3>R$ 1.205</h3>
+                        </div>
+                    </div>
                 </section>
 
                 <section className="informacoes">
                     <h1>Por que buscar conosco? </h1>
                     <div className="cartoes">
                         <div className="card">
-                            <BiSearchAlt2 style={{opacity:"50%",fill:"var(--preto)"}} size={100}/>
+                            <BiSearchAlt2 style={{ opacity: "50%", fill: "var(--preto)" }} size={100} />
                             <div className="texto-card">
                                 <h4>Lorem Ipsu </h4>
 
@@ -45,7 +83,7 @@ function Home(){
                         </div>
 
                         <div className="card">
-                            <TiLocation style={{opacity:"50%" , fill:"var(--preto)"}} size={100}/>
+                            <TiLocation style={{ opacity: "50%", fill: "var(--preto)" }} size={100} />
                             <div className="texto-card">
                                 <h4>Lorem Ipsu </h4>
 
@@ -54,7 +92,7 @@ function Home(){
                         </div>
 
                         <div className="card">
-                            <BiSearchAlt2 style={{opacity:"50%",fill:"var(--preto)"}} size={100}/>
+                            <BiSearchAlt2 style={{ opacity: "50%", fill: "var(--preto)" }} size={100} />
                             <div className="texto-card">
                                 <h4>Lorem Ipsu </h4>
 
@@ -63,11 +101,18 @@ function Home(){
                         </div>
                     </div>
                 </section>
+
                 <footer>
                     <p>Meu Melhor Imovél &copy; 2021</p>
                 </footer>
+
             </main>
         </>
     );
 }
 export default Home;
+
+const ativaMenu = () => {
+    document.getElementsByClassName("btnFormBusca")?.className.toggle('btnOff');
+}
+
