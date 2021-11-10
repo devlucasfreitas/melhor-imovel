0bc30083ce73cf23256e68abc39618e294db2ef2
João Antônio <65183458+scjoaoantonio@users.noreply.github.com>
2021-11-10 12:25:10 -0300

    Add files via upload
    new cards

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/header';
 import './home.css';
 import { BiSearchAlt2 } from "react-icons/bi";
-import { TiLocation } from "react-icons/ti";
+import { TiLocation } from "react-icons/ti"
 
 
 function Home() {
@@ -15,54 +15,66 @@ function Home() {
             <main className="Home">
 
                 <section className="painel">
+                    
+                    <form className="formBusca" action="">
 
-                        <h1 className="titulo">A melhor busca para o seu <br />melhor imóvel</h1>
+                        <h1 className="titulo">A melhor busca para o seu melhor imóvel</h1>
 
                         <div><button onClick={ativaMenu} className="btnFormBusca ">Começar a Busca</button></div>
 
                         <div className="selecaoBox ">
-                            <select name="alugaOuCompra">
-                                <option value="Alugar">Alugar</option>
-                                <option value="Comprar">Comprar</option>
-                            </select>
-                            <select name="tiposDePropriedade">
-                                <option value="todos">Mostrar todos</option>
-                                <option value="apartamento">Somente Apartamento</option>
-                                <option value="casa">Somente Casa</option>
-                            </select>
-                            <select name="estado">
-                                <option value="SP">SP</option>
-                            </select>
-                            <select name="cidade">
-                                <option value="SAO PAULO">São Paulo</option>
-                            </select>
-                            <select name="bairro">
-                                <option value="JARDINS">Jardins</option>
-                            </select>
+                        <select name="alugaOuCompra">
+                            <option value="Alugar">Alugar</option>
+                            <option value="Comprar">Comprar</option>
+                        </select>
+                        <select name="tiposDePropriedade">
+                            <option value="todos">Mostrar todos</option>
+                            <option value="apartamento">Somente Apartamento</option>
+                            <option value="casa">Somente Casa</option>
+                        </select>
+                        <select name="estado">
+                            <option value="SP">SP</option>
+                        </select>
+                        <select name="cidade">
+                            <option value="SAO PAULO">São Paulo</option>
+                        </select>
+                        <select name="bairro">
+                            <option value="JARDINS">Jardins</option>
+                        </select>
 
-                            <button className="btnBusca">Buscar</button>
+                        <button className="btnBusca">Buscar</button>
                         </div>
-
+                    </form>
+                    
                 </section>
+
 
 
                 <section className="preco-medio">
 
-                    <div className="preco-aluguel"><h1>Preço Médio de aluguel</h1></div>
+                      <div className="bloco-precos">
 
-                    <div className="bloco-precos">
+                        <div className="cards price_card">
+                            <p> PREÇO MÉDIO POR REGIÃO</p>
+                        </div>
+
                         <div className="cards sp">
-                            <h2>São paulo</h2>
-                            <h3>R$ 3.476</h3>
+                            <h4>São Paulo - SP</h4>
                         </div>
                         <div className="cards rj">
-                            <h2>Rio de Janeiro</h2>
-                            <h3>R$ 1.808</h3>
+                            <h4>Rio de Janeiro - RJ</h4>
                         </div>
                         <div className="cards pr">
-                            <h2>Curitiba</h2>
-                            <h3>R$ 1.205</h3>
+                            <h4>Curitiba - PR </h4>
                         </div>
+                        <div className="cards fl">
+                            <h4>Florianópolis - SC </h4>
+                        </div>
+
+                        <div className="cards plus_card">
+                            <p>+</p>
+                        </div>
+
                     </div>
                 </section>
 
@@ -109,5 +121,6 @@ function Home() {
 export default Home;
 
 const ativaMenu = () => {
-    document.getElementsByClassName("btnFormBusca").className.toggle('btnOff');
+    document.getElementsByClassName("btnFormBusca")?.className.toggle('btnOff');
 }
+
